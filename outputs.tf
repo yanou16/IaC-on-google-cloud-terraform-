d01b16883f74b8c402a2b922c29c439dccfc3366
yanou16 <rayanelouzazna13@gmail.com>
2025-09-13 17:32:52 +0100

    add ansible to the app

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
 output "public_ip" {
   value = google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip
 }
+output "ansible_command" {
+  value = "cd ansible && ansible-playbook -i inventory.ini setup.yml"
+}
